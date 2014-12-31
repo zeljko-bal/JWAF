edd2ef5385e5b30d36d9317bb46fdcf3cba3c316
zeljko.bal <zeljko.bal@gmail.com>
2014-12-31 13:42:06 +0100

    - implemented SerializationUtils

--- src/main/java/org/jwaf/util/SerializationUtils.java
+++ src/main/java/org/jwaf/util/SerializationUtils.java
@@ -1,18 +1,55 @@
 package org.jwaf.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import com.sun.jersey.core.util.Base64;
 
 public class SerializationUtils
 {
-	public static Serializable deSerialize(String s)
+	public static Serializable deSerialize(String string)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		byte[] bytes = Base64.decode(string.getBytes());
+		Serializable object = null;
+		
+		try 
+		{
+			ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(bytes));
+			object = (Serializable) objectInputStream.readObject();
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} 
+		catch (ClassCastException e) {
+			e.printStackTrace();
+		}
+		
+		return object;
 	}
-	
-	public static String serialize(Serializable o)
+
+	public static String serialize(Serializable object)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		String encoded = null;
+
+		try
+		{
+			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+			objectOutputStream.writeObject(object);
+			objectOutputStream.close();
+			encoded = new String(Base64.encode(byteArrayOutputStream.toByteArray()));
+		} 
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+		return encoded;
 	}
 }
